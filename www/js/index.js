7971b4c209c6738fe5257352a9f3111a929f4e2a
mulyoved <mulyoved@gmail.com>
2013-06-24 10:04:54 +0300

    try debug1

--- www/js/index.js
+++ www/js/index.js
@@ -37,9 +37,12 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        console.log('Received Event: ');
+        
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
+
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
